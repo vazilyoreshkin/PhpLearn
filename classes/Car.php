52debd84b2e85c9d0c9849a7a49520896b28f588
Vazily <vazily2015@gmail.com>
2021-07-18 20:03:25 +0100

    php netbeam

--- classes/Car.php
+++ classes/Car.php
@@ -3,11 +3,27 @@
 class Car
 {
     public $color;
-    public $whels = 4;
-    public $speed = 180;
-    var $brand;
-
-
+    public $whels;
+    public $speed;
+    public $brand;
+   
+//    public function __construct($color, $wheels = 4, $speed, $brand) 
+//    {
+//        $this->color = $color;
+//        $this->wheels = $wheels;
+//        $this->speed = $speed;
+//        $this->brand = $brand;
+//        echo __me;
+//
+//    }
+    public function getCarInfo() {
+    
+        return "<h3>О моем авто:</h3>
+        Марка: {$this->brand}<br>
+        Цвет:{$this->color}<br>
+        Кол-во колес:{$this->whels}<br>
+        Скорость:{$this->speed}";
+            }
 }
 
 
--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php
+
+error_reporting(-1);
 require_once 'classes/Car.php';
 
 function debug($data)
 {
     echo '<pre>' . print_r($data, 1) .'</pre>';
 }
-    ;
-
-$car1 = new Car();
-$car2 = new Car();
-
-debug($car1);
-
-$car1->color = 'черный';
-$car1->brand = 'renault';
-$car1->speed = 200;
-$car1->year = 2018;
-
-$car2->color = 'белый';
-$car2->brand = 'mercrdes';
-$car2->speed = 120;
-$car2->year = 2017;
-
-debug($car1);
-debug($car2);
+$car1 = new Car('черный', 4, 180, 'renault');
+$car2 = new Car('черный', 4, 200, 'bmw');
 
 
-
-
+echo $car1->getCarInfo();
+echo $car2->getCarInfo();
